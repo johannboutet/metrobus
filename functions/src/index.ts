3572e2b5799a5744d77df3a4dc3789ce889be301
Johann Boutet <contact@johannboutet.com>
2018-03-11 12:53:21 -0400

    Ask user to get device location

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,50 +1,62 @@
-import * as functions from 'firebase-functions';
 import * as actions from 'actions-on-google';
-import * as request from 'request';
+import * as functions from 'firebase-functions';
 import { BusLineStopTime } from 'models/rtc_responses';
+import * as request from 'request';
 
 process.env.DEBUG = 'actions-on-google:*';
 
 const App = actions.DialogflowApp;
 
-// a. the action name from the make_name Dialogflow intent
-const NAME_ACTION = 'input.welcome';
+const metroBusActions = {
+  WELCOME_ACTION: 'input.welcome',
+  GET_STOP_TIMES_ACTION: 'get_stop_times',
+};
 
-// b. the parameters that are parsed from the make_name intent
-// const COLOR_ARGUMENT = 'color';
-// const NUMBER_ARGUMENT = 'number';
-
-export const getBusSchedule = functions.https.onRequest((req, resp) => {
+export const metroBusFulfillment = functions.https.onRequest((req, resp) => {
   const app = new App({ request: req, response: resp });
 
-  function getSchedule(app) {
-    request('https://wsmobile.rtcquebec.ca/api/v2/horaire/ListeBorneVirtuelle_Arret?source=appmobileios&arrets=2557', function (error, response, body) {
-      if (error || (response && response.statusCode !== 200)) {
-        sorry();
-        return;
-      }
+  const requestLocation = (app) => {
+    app.askForPermission('Pour trouver les arrêts de bus proches de vous', app.SupportedPermissions.DEVICE_PRECISE_LOCATION);
+  };
 
-      const schedules: BusLineStopTime[] = JSON.parse(body);
-      const busLine = schedules.find((line) => line.parcours.noParcours === '800');
+  const getStopTimes = (app) => {
+    if (app.isPermissionGranted()) {
+      const deviceCoordinates = app.getDeviceLocation().coordinates;
+      console.log('location', deviceCoordinates);
 
-      if (!busLine) {
-        sorry();
-        return;
-      }
+      request('https://wsmobile.rtcquebec.ca/api/v2/horaire/ListeBorneVirtuelle_Arret?source=appmobileios&arrets=2557', function (error, response, body) {
+        if (error || (response && response.statusCode !== 200)) {
+          sorry();
+          return;
+        }
 
-      const times = busLine.horaires.map((schedule) => schedule.departMinutes).sort((a, b) => a - b).join(', ');
-      const name = `${busLine.parcours.noParcours} direction ${busLine.parcours.descriptionDirection}`;
+        const schedules: BusLineStopTime[] = JSON.parse(body);
+        const busLine = schedules.find((line) => line.parcours.noParcours === '800');
 
-      app.tell(`Le bus ${name} passe dans ${times} minutes.`);
-    });
-  }
+        if (!busLine) {
+          sorry();
+          return;
+        }
 
-  function sorry() {
+        const times = busLine.horaires.map((schedule) => schedule.departMinutes).sort((a, b) => a - b).join(', ');
+        const name = `${busLine.parcours.noParcours} direction ${busLine.parcours.descriptionDirection}`;
+
+        app.tell(`Le bus ${name} passe dans ${times} minutes.`);
+      });
+    }
+    else {
+      app.tell('Désolé, je n\'ai pas pu vous localiser.');
+    }
+  };
+
+  const sorry = () => {
     app.tell('Désolé, je n\'ai pas pu trouver les horaires pour ce bus.');
-  }
+  };
 
   const actionMap = new Map();
-  actionMap.set(NAME_ACTION, getSchedule);
+
+  actionMap.set(metroBusActions.WELCOME_ACTION, requestLocation);
+  actionMap.set(metroBusActions.GET_STOP_TIMES_ACTION, getStopTimes);
 
   app.handleRequest(actionMap);
 });
