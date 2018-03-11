66962cbeef35822de6e809f49848a87666a89a1d
Johann Boutet <contact@johannboutet.com>
2018-03-11 18:39:03 -0400

    Store favorite bus line

--- functions/src/metrobus-fulfillment.ts
+++ functions/src/metrobus-fulfillment.ts
@@ -6,14 +6,19 @@ import * as request from 'request';
 
 const metrobusActions = {
   WELCOME: 'input.welcome',
+  REQUEST_LOCATION: 'request_location',
   STORE_LOCATION: 'store_location',
+  ASK_FAVORITE_BUS_LINE: 'ask_favorite_bus_line',
+  STORE_FAVORITE_BUS_LINE: 'store_favorite_bus_line',
+  GET_FAVORITE_BUS_LINE: 'get_favorite_bus_line',
+  FAVORITE_BUS_LINE_FOLLOW_UP_NO: 'favorite_bus_line_follow_up_no',
   GET_STOP_TIMES: 'get_stop_times',
 };
 
 export class MetrobusFulfillment {
   private app: DialogflowApp;
-  private permissions;
   private userStorage: UserInterface;
+  private permissions;
 
   constructor(req: Request, res: Response) {
     this.app = new DialogflowApp({
@@ -21,19 +26,80 @@ export class MetrobusFulfillment {
       response: res,
     });
 
-    this.permissions = this.app.SupportedPermissions;
     this.userStorage = this.app.userStorage as UserInterface;
+    this.permissions = this.app.SupportedPermissions;
   }
 
   run() {
     const actionMap = new Map();
 
-    actionMap.set(metrobusActions.WELCOME, this.requestLocation);
+    actionMap.set(metrobusActions.WELCOME, this.welcome);
+
+    actionMap.set(metrobusActions.REQUEST_LOCATION, this.requestLocation);
     actionMap.set(metrobusActions.STORE_LOCATION, this.storeLocation);
+
+    actionMap.set(metrobusActions.ASK_FAVORITE_BUS_LINE, this.askFavoriteBusLine);
+    actionMap.set(metrobusActions.STORE_FAVORITE_BUS_LINE, this.storeFavoriteBusLine);
+    actionMap.set(metrobusActions.GET_FAVORITE_BUS_LINE, this.getFavoriteBusLine);
+    actionMap.set(metrobusActions.FAVORITE_BUS_LINE_FOLLOW_UP_NO, this.favoriteBusLineFollowUpNo);
+
     actionMap.set(metrobusActions.GET_STOP_TIMES, this.getStopTimes);
 
     this.app.handleRequest(actionMap);
   }
+
+  welcome = () => {
+    if (this.userStorage.location && this.userStorage.favoriteBusLine) {
+      this.getStopTimes();
+    }
+    else if (!this.userStorage.location && this.userStorage.favoriteBusLine) {
+      this.requestLocation();
+    }
+    else if (this.userStorage && !this.userStorage.favoriteBusLine) {
+      this.askFavoriteBusLine();
+    }
+    else if (!this.userStorage.location && !this.userStorage.favoriteBusLine) {
+      // TODO
+    }
+  };
+
+  askFavoriteBusLine = () => {
+    this.app.ask('Quelle est votre ligne de bus favorite ?');
+  };
+
+  storeFavoriteBusLine = () => {
+    const lineNumber = +this.app.getArgument('bus_line');
+    const line = {
+      number: lineNumber,
+      direction: 'ouest',
+    };
+
+    this.userStorage.favoriteBusLine = line;
+
+    this.app.tell(`Votre ligne favorite est maintenant le bus ${line.number} direction ${line.direction}.`);
+  };
+
+  getFavoriteBusLine = () => {
+    const line = this.userStorage.favoriteBusLine;
+
+    if (line) {
+      this.app.ask(`Votre ligne favorite est le bus ${line.number} direction ${line.direction}. Voulez-vous la changer ?`);
+    }
+    else {
+      this.app.ask('Je ne connais pas encore votre ligne de bus favorite. Voulez-vous la personnaliser maintenant ?');
+    }
+  };
+
+  favoriteBusLineFollowUpNo = () => {
+    const line = this.userStorage.favoriteBusLine;
+
+    if (line) {
+      this.app.tell(`D'accord, votre ligne favorite reste le bus ${line.number} direction ${line.direction}.`);
+    }
+    else {
+      this.app.tell('D\'accord, demandez-moi plus tard si vous voulez la changer.');
+    }
+  };
 
   requestLocation = () => {
     this.app.askForPermission('Pour trouver les arrêts de bus proches de vous', this.permissions.DEVICE_PRECISE_LOCATION);
@@ -43,10 +109,10 @@ export class MetrobusFulfillment {
     if (this.app.isPermissionGranted()) {
       this.userStorage.location = this.app.getDeviceLocation().coordinates;
 
-      this.app.tell('Merci, je peux maintenant vous donner les horaires de passage aux arrêts de bus proches de chez vous. Vous pouvez me redemander de vous localiser à tout moment pour mettre à jour votre position.')
+      this.app.tell('J\'ai bien enregistré votre position. Vous pouvez me redemander de vous localiser à tout moment pour mettre à jour votre position.');
     }
     else {
-      this.app.tell('Je ne peux pas vous donner les horaires de passage aux arrêts de bus proches de chez vous si je n\'ai pas votre localisation.');
+      this.app.tell('Je ne pourrai pas vous donner les horaires de passage aux arrêts de bus proches de chez vous si je n\'ai pas votre localisation.');
     }
   };
 
